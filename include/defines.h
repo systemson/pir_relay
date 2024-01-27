1a60ed2a155e7257c22b12d017ee1418431e001b
Deivi Peña <dpena@bankingtech.com.ve>
2024-01-27 14:10:34 -0400

    Adding new variables

--- include/defines.h
+++ include/defines.h
@@ -1,36 +1,36 @@
 // A (pseudo) integer key, in the form of #define which is handled
 // by the preprocessor and not your program:
-#define SYS_MAINTENANCE  0
-#define SYS_GROUP        1
-#define SYS_TYPE         2
-#define SYS_DESC         3
-#define SYS_ACTION       4
-#define SYS_HARD_RESET   5
+#define SYS_MAINTENANCE      0
+#define SYS_GROUP            1
+#define SYS_BOARD            2
+#define SYS_DESC             3
+#define SYS_ACTION           4
+#define SYS_HARD_RESET       5
 
-#define WIFI_SSID        6
-#define WIFI_PASS        7
-#define IP_ADDRESS       8
-#define MAC_ADDRESS      9
-#define NONE_10         10
+#define WIFI_SSID            6
+#define WIFI_PASS            7
+#define IP_ADDRESS           8
+#define MAC_ADDRESS          9
+#define NONE_10             10
 
-#define MQTT_BROKER     11
-#define MQTT_PORT       12
-#define MQTT_PASS       13
-#define MQTT_USER       14
-#define MQTT_PUB_TOPIC  15
-#define MQTT_SUB_TOPIC  16
-#define MQTT_CONF_TOPIC 17
+#define MQTT_BROKER         11
+#define MQTT_PORT           12
+#define MQTT_PASS           13
+#define MQTT_USER           14
+#define MQTT_PUB_TOPIC      15
+#define MQTT_SUB_TOPIC      16
+#define MQTT_CONF_TOPIC     17
 
 
-#define NONE_18         18
-#define NONE_19         19
+#define NONE_18             18
+#define NONE_19             19
 
-#define MAX_DELAY       20
-#define LOOP_TIME       21
-#define FORCED_ON       22
-#define NONE_23         23
-#define NONE_24         24
-#define FIRM_VERSION    25
+#define MAX_DELAY           20
+#define LOOP_TIME           21
+#define FORCED_ON           22
+#define NONE_23             23
+#define UPDATE_URL          24
+#define FIRMWARE_VERSION    25
 
 
 #define CONFIG_SIZE     25
@@ -52,4 +52,3 @@
 
 #define S2_PIN           9
 #define S3_PIN          10
-
